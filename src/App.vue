7c47c512f1d71579ac2843e8e98d6d939100c1e0
Furkan Yanteri <furkanyanteri@gmail.com>
2024-07-09 14:09:38 +0300

    init: vuex store, router and home view added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  App.Vue
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  components: {
-  }
 }
 </script>
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Home View</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'HomeView',
+  computed: {
+    ...mapState(['count'])
+  },
+  methods: {
+    ...mapActions(['increment'])
+  }
+}
+</script>
+
+<style scoped>
+/* Add styles here */
+</style>
