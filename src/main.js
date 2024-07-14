89180937e2232800e261af847e8cef03b9e20585
Furkan Yanteri <furkanyanteri@gmail.com>
2024-07-14 12:22:02 +0300

    feat: added fas

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/assets/styles.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { 
+    faHome,
+    faUser,
+    faEnvelope,
+    faPhoneAlt,
+    faClock 
+} from '@fortawesome/free-solid-svg-icons' // Use free-solid-svg-icons for solid icons
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(router).use(store).mount('#app')
+library.add(faHome, faUser, faEnvelope, faPhoneAlt, faClock)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(store)
+app.use(router)
+
+app.mount('#app')
