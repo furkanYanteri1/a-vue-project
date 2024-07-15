b123c860248779c77529d09abc5dcd0c19432ce9
Furkan Yanteri <furkanyanteri@gmail.com>
2024-07-15 18:15:05 +0300

    feat: alternating row colors

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,32 +1,45 @@
 <template>
   <div class="data-table">
     <AppointmentRecord
-      v-for="record in records"
+      v-for="(record, index) in records"
       :key="record.id"
       :record="record"
+      :class="{
+        'even-row': index % 2 === 0,
+        'odd-row': index % 2 !== 0
+      }"
     />
   </div>
 </template>
 
 <script>
-import AppointmentRecord from '@/components/AppointmentRecord.vue'
+import AppointmentRecord from "@/components/AppointmentRecord.vue";
 
 export default {
-  name: 'DataTable',
+  name: "DataTable",
   components: {
-    AppointmentRecord
+    AppointmentRecord,
   },
   props: {
     records: {
       type: Array,
-      required: true
-    }
-  }
-}
+      required: true,
+    },
+  },
+};
 </script>
 
 <style scoped>
 .data-table {
   width: 100%;
+}
+
+/* Alternating row colors */
+.even-row {
+  background-color: white;
+}
+
+.odd-row {
+  background-color: rgb(188, 188, 188);
 }
 </style>
