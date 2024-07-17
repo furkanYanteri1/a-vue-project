9290e314211b5a54fa5161962c9391ca818eac81
Furkan Yanteri <furkanyanteri@gmail.com>
2024-07-17 19:49:54 +0300

    refactor: agent color logic refined, removed unnecessary functions

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,6 @@ const store = createStore({
     appointments: [],
     filteredAppointments: [],
     agents: [],
-    agentColorMap: {},
     agentFilter: [],
     activeStatus: "All Statuses",
     count: 0,
@@ -21,9 +20,6 @@ const store = createStore({
     },
     setAgents(state, agents) {
       state.agents = agents;
-    },
-    setAgentColorMap(state, agentColorMap) {
-      state.agentColorMap = agentColorMap;
     },
     setAgentFilter(state, agentFilter) {
       state.agentFilter = agentFilter;
@@ -103,10 +99,6 @@ const store = createStore({
         commit("setAppointments", records);
         commit("setAgents", agents);
 
-        // Update agent color map after fetching appointments
-        const agentColorMap = createAgentColorMap(records);
-        commit("setAgentColorMap", agentColorMap);
-
         // Create agent filter using getters directly
         const agentFilters = createAgentFilter(records, getters);
         commit("setAgentFilter", agentFilters);
@@ -122,7 +114,8 @@ const store = createStore({
     appointments: (state) => state.appointments,
     count: (state) => state.count,
     agentColor: (state) => (agentId) => {
-      return state.agentColorMap[agentId] || "gray"; // just in case the map is missing that id
+      const agent = state.agents.find(x=>x.id === agentId)
+      return ({...agent.fields}.color);
     },
     agentFilter: (state) => state.agentFilter,
     activeStatus: (state) => state.activeStatus,
@@ -133,23 +126,6 @@ const store = createStore({
 });
 
 // HELPER FUNCTIONS---------------------------------------------------------------
-// function to create [random unique color]-[agent] pairs
-function createAgentColorMap(appointments) {
-  const agentColorMap = {};
-
-  appointments.forEach((appointment) => {
-    const agentIds = appointment.fields.agent_id || []; // Get agent_id array or default to empty array
-    const agentColor = getRandomColor(); // Generate a random color for each agent
-
-    agentIds.forEach((agentId) => {
-      if (agentId && !agentColorMap[agentId]) {
-        agentColorMap[agentId] = agentColor;
-      }
-    });
-  });
-
-  return agentColorMap;
-}
 
 // Function to create agent filter list
 function createAgentFilter(records, getters) {
@@ -170,26 +146,6 @@ function createAgentFilter(records, getters) {
   });
 
   return agentFilters;
-}
-
-// Function to generate a random hexadecimal color
-// If the color is gray, repicking the color to avoid potential visual problem with the avatar for more
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  let isGray = true;
-
-  while (isGray) {
-    color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    // Check if the color is not a shade of gray
-    isGray =
-      /^#(?:[0-9a-fA-F]{3}){1,2}$/.test(color) && color.match(/^#(.)\1{2}/);
-  }
-
-  return color;
 }
 
 function getTimeDiff(appointmentDate) {
